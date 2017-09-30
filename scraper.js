c3df6e8a505fd09cab196280d5decadfacd0a576
imears <maxwellmears@gmail.com>
2017-09-29 21:20:01 -0700

    Extract scraper into a new Scraper class

---
+++ scraper.js
@@ -0,0 +1,42 @@
+const puppeteer = require('puppeteer');
+
+class Scraper {
+  constructor(passenger) {
+    this.passenger = passenger;
+  }
+
+  async run() {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    const { confirmationNumber, firstName, lastName } = this.passenger;
+
+    // Visit Southwest check in page
+    await page.goto('https://www.southwest.com/air/check-in');
+
+    // Fill in confirmation number
+    await page.click('#confirmationNumber');
+    await page.type(confirmationNumber);
+
+    // Fill in first name
+    await page.click('#passengerFirstName');
+    await page.type(firstName);
+
+    // Fill in last name
+    await page.click('#passengerLastName');
+    await page.type(lastName);
+
+    // Click submit
+    await page.click('#form-mixin--submit-button');
+
+    // Wait for page to load
+    await page.waitForSelector('.message_error');
+
+    // Take screenshot
+    await page.screenshot({ path: 'southwest-check-in.png', fullPage: true });
+
+    await browser.close();
+  }
+}
+
+module.exports = Scraper;
