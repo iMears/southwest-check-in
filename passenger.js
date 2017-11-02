5975abe191612c06d0df7f9bd6363851e4505f36
imears <maxwellmears@gmail.com>
2017-11-02 16:45:18 -0700

    Send boarding pass via text

--- passenger.js
+++ passenger.js
@@ -2,6 +2,7 @@ const passenger = {
   confirmationNumber: 'XXXXXX',
   firstName: 'Maxwell',
   lastName: 'Mears',
+  phoneNumber: '5555555555',
 };
 
 module.exports = passenger;
--- scraper.js
+++ scraper.js
@@ -1,3 +1,4 @@
+
 const puppeteer = require('puppeteer');
 
 class Scraper {
@@ -9,7 +10,7 @@ class Scraper {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
-    const { confirmationNumber, firstName, lastName } = this.passenger;
+    const { confirmationNumber, firstName, lastName, phoneNumber } = this.passenger;
 
     // Visit Southwest check in page
     await page.goto('https://www.southwest.com/air/check-in');
@@ -30,7 +31,20 @@ class Scraper {
     await page.click('#form-mixin--submit-button');
 
     // Wait for page to load
-    await page.waitForSelector('.message_error');
+    await page.waitForSelector('.air-check-in-review-results--confirmation');
+
+    // Click 'Check In'
+    await page.click('.air-check-in-review-results--check-in-button');
+
+    // Click 'Text' to have boarding pass sent via text
+    await page.click('.boarding-pass-options--button-text');
+
+    // Enter phone number
+    await page.click('#textBoardingPass');
+    await page.type(phoneNumber);
+
+    // Click 'Send' to send boarding pass test
+    await page.click('#form-mixin--submit-button');
 
     // Take screenshot
     await page.screenshot({ path: 'southwest-check-in.png', fullPage: true });
