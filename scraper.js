46b105170b20aa76c0250302e687bf34cddb9332
imears <maxwellmears@gmail.com>
2017-11-02 18:26:43 -0700

    Add support for email boarding pass delivery method

--- scraper.js
+++ scraper.js
@@ -1,4 +1,3 @@
-
 const puppeteer = require('puppeteer');
 
 class Scraper {
@@ -10,8 +9,14 @@ class Scraper {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
-    const { confirmationNumber, firstName, lastName, phoneNumber } = this.passenger;
-
+    const {
+      confirmationNumber,
+      firstName,
+      lastName,
+      phone,
+      email,
+      boardingPassDeliveryMethods,
+    } = this.passenger;
     // Visit Southwest check in page
     await page.goto('https://www.southwest.com/air/check-in');
 
@@ -36,15 +41,29 @@ class Scraper {
     // Click 'Check In'
     await page.click('.air-check-in-review-results--check-in-button');
 
-    // Click 'Text' to have boarding pass sent via text
-    await page.click('.boarding-pass-options--button-text');
+    if (boardingPassDeliveryMethods.includes('text')) {
+      // Click 'Text' to have boarding pass sent via text
+      await page.click('.boarding-pass-options--button-text');
 
-    // Enter phone number
-    await page.click('#textBoardingPass');
-    await page.type(phoneNumber);
+      // Enter phone number
+      await page.click('#textBoardingPass');
+      await page.notificationType(phone);
 
-    // Click 'Send' to send boarding pass test
-    await page.click('#form-mixin--submit-button');
+      // Click 'Send' to send boarding pass email
+      await page.click('#form-mixin--submit-button');
+    }
+
+    if (boardingPassDeliveryMethods.includes('email')) {
+      // Click 'Email' to have boarding pass sent via email
+      await page.click('.boarding-pass-options--button-email');
+
+      // Enter email address
+      await page.click('#emailBoardingPass');
+      await page.type(email);
+
+      // Click 'Send' to send boarding pass email
+      await page.click('#form-mixin--submit-button');
+    }
 
     // Take screenshot
     await page.screenshot({ path: 'southwest-check-in.png', fullPage: true });
